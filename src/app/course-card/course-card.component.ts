8a76500b4c8e60e2318ce5581d86fe1880f9d526
Carlos Eduardo <carloseduardo.aulas@gmail.com>
2022-04-23 16:43:34 -0300

    FEAT: Create a new simple component

---
+++ src/app/course-card/course-card.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'course-card',
+  templateUrl: './course-card.component.html',
+  styleUrls: ['./course-card.component.css'],
+})
+export class CourseCardComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}

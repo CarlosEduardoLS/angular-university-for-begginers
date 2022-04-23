2d4a5a025f86954694524a47bc988e7c77fb25da
Carlos Eduardo <carloseduardo.aulas@gmail.com>
2022-04-23 19:12:04 -0300

    FEAT: Declare new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { CourseCardExamplesComponent } from './course-card-examples/course-card-examples.component';
 import { CourseCardComponent } from './course-card/course-card.component';
+import { CourseImageComponent } from './course-image/course-image.component';
 
 @NgModule({
-  declarations: [AppComponent, CourseCardComponent],
+  declarations: [
+    AppComponent,
+    CourseCardComponent,
+    CourseImageComponent,
+    CourseCardExamplesComponent,
+  ],
   imports: [BrowserModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],

972762b691c861c564931bbddd0dff1a0abc2662
Carlos Eduardo <carloseduardo.aulas@gmail.com>
2022-04-23 19:11:48 -0300

    FEAT: Removed code unused by this section of course

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,18 +9,4 @@ import { Course } from './model/course';
 })
 export class AppComponent {
   courses = COURSES;
-
-  title = 'angular core deep dive';
-
-  price = 9.99294249294294;
-
-  rate = 0.67;
-
-  course = COURSES[0];
-
-  startDate = new Date(2000, 0, 1);
-
-  getSelectedCourse(course: Course) {
-    console.log('Selected Course -> ', course);
-  }
 }

74142c21b9af013263243c7230ceeb69d10cbc3b
Carlos Eduardo <carloseduardo.aulas@gmail.com>
2022-04-23 16:43:49 -0300

    FEAT: Declarete a new component on module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { CourseCardComponent } from './course-card/course-card.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, CourseCardComponent],
   imports: [BrowserModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],

0ba489ba338940305e14f77dca1cf3aad9830c19
Carlos Eduardo <carloseduardo.aulas@gmail.com>
2022-04-23 17:05:54 -0300

    FEAT: Create a new variable to recieve array of courses

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { COURSES } from '../db-data';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  courses = COURSES;
+}

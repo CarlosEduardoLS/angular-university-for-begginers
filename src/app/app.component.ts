426d8e464f050646f3b5c2d7973bf182e2557c4a
Carlos Eduardo <carloseduardo.aulas@gmail.com>
2022-04-23 16:37:50 -0300

    FEAT: Included new files and configurations used by the course

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,10 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'Angular University';
-
-  onKeyUp(newTitle: string) {
-    this.title = newTitle;
-  }
-}
+export class AppComponent {}

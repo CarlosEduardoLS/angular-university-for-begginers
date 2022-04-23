320c3348f94aece7f514fd88414178d8f35630e8
Carlos Eduardo <carloseduardo.aulas@gmail.com>
2022-04-23 16:10:50 -0300

    FEAT: Craete function to real time update works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-university-for-begginers';
+  title = 'Angular University';
+
+  onKeyUp(newTitle: string) {
+    this.title = newTitle;
+  }
 }

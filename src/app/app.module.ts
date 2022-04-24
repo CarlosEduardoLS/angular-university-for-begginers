b8cdaf4d16e8ccf3df16dc5d5c396e54303f658e
Carlos Eduardo <carloseduardo.aulas@gmail.com>
2022-04-24 16:42:01 -0300

    FIX: Adjusted the importations of components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
-import { CourseCardExamplesComponent } from './course-card-examples/course-card-examples.component';
-import { CourseCardComponent } from './course-card/course-card.component';
-import { CourseImageComponent } from './course-image/course-image.component';
 import { HttpClientModule } from '@angular/common/http';
 import { CoursesService } from '../services/courses.service';
+import { CourseCardComponent } from './course-card/course-card.component';
+import { CourseImageComponent } from './course-image/course-image.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CourseCardComponent,
-    CourseImageComponent,
-    CourseCardExamplesComponent,
-  ],
+  declarations: [AppComponent, CourseCardComponent, CourseImageComponent],
   imports: [BrowserModule, BrowserAnimationsModule, HttpClientModule],
   providers: [CoursesService],
   bootstrap: [AppComponent],
